4b0e9dc0def81329e781ad0c7dc34fe16016cc34
rizki-ap <rizki.ap@gmail.com>
2021-05-24 17:31:40 +0700

    Update ph-ec-monitoring.ino

--- arduino/ph-ec-monitoring.ino
+++ arduino/ph-ec-monitoring.ino
@@ -1,6 +1,7 @@
 #define CONST_PH_MUL -5.70
 #define CONST_PH_ADD 21.34
 #define TDS_FACTOR 0.5
+#define POWER_SETTLE_TIME 1000
 
 const int pHAnalogPin = A0; 
 const int pHEnablePin = 5; 
@@ -11,18 +12,21 @@ const int ECEnablePin = 6;
 void setup() {
   //setting Serial comm
   Serial.begin(115200);
+  
   //setting ph enable pin
   pinMode(pHEnablePin, OUTPUT);
-  digitalWrite(pHEnablePin, HIGH);
+  digitalWrite(pHEnablePin, HIGH); //high is OFF
+  
   //setting ec enable pin
   pinMode(ECEnablePin, OUTPUT);
-  digitalWrite(ECEnablePin, HIGH);
+  digitalWrite(ECEnablePin, HIGH); //high is OFF
 }
 
 void loop() {
   float sensorPH = bacaPH();
   Serial.print("sensor pH = ");
   Serial.println(sensorPH);
+  
   float sensorTDS = bacaTDS(25.0);
   Serial.print("sensor TDS (ppm)= ");
   Serial.println(sensorTDS);
@@ -32,15 +36,15 @@ float bacaEC(float waterTemperature){
   int buf[10],temp;
   unsigned long int avgValue=0; 
 
-  digitalWrite(ECEnablePin, LOW);
-  delay(1000);
+  digitalWrite(ECEnablePin, LOW);   // enable EC module power
+  delay(POWER_SETTLE_TIME);         // wait for power to settle
 
   for(int i=0;i<10;i++) { 
-    buf[i]=analogRead(ECAnalogPin);
+    buf[i]=analogRead(ECAnalogPin); //read sensor 10 times
     delay(10);
   }
   for(int i=0;i<9;i++){
-    for(int j=i+1;j<10;j++){
+    for(int j=i+1;j<10;j++){  //sorting the reading
       if(buf[i]>buf[j]){
         temp=buf[i];
         buf[i]=buf[j];
@@ -48,13 +52,13 @@ float bacaEC(float waterTemperature){
       }
     }
   }
-  for(int i=1;i<9;i++) avgValue+=buf[i];
+  for(int i=1;i<9;i++) avgValue+=buf[i]; //eliminate the lowest and highest and summing 8-data
   
-  float voltage = (float)avgValue*5.0/1024/8;
+  float voltage = (float)avgValue*5.0/1024/8; // averaging by 8
   float ecValue = 133.42*voltage*voltage*voltage - 255.86*voltage*voltage + 857.39*voltage;
   float ecValue25 = ecValue / (1.0+0.02*(waterTemperature-25.0));  //temperature compensation
 
-  digitalWrite(ECEnablePin, HIGH);
+  digitalWrite(ECEnablePin, HIGH); // disable ED module power
   return(ecValue25); 
 }
 
@@ -64,17 +68,17 @@ float bacaTDS(float waterTemperature){
 
 float bacaPH() {
   int buf[10],temp;
-  unsigned long int avgValue=0; 
+  unsigned long int avgValue=0; //read sensor 10 times
 
-  digitalWrite(pHEnablePin, LOW);
-  delay(1000);
+  digitalWrite(pHEnablePin, LOW); // enable PH module power
+  delay(POWER_SETTLE_TIME);       // wait for power to settle           
   
   for(int i=0;i<10;i++) { 
     buf[i]=analogRead(pHAnalogPin);
     delay(10);
   }
   for(int i=0;i<9;i++){
-    for(int j=i+1;j<10;j++){
+    for(int j=i+1;j<10;j++){      //sorting the reading
       if(buf[i]>buf[j]){
         temp=buf[i];
         buf[i]=buf[j];
@@ -82,11 +86,11 @@ float bacaPH() {
       }
     }
   }
-  for(int i=1;i<9;i++) avgValue+=buf[i];
+  for(int i=1;i<9;i++) avgValue+=buf[i]; //discard the lowest and highest and summing 8-data
   
   float pHVol = (float)avgValue*5.0/1024/8;
   float phValue = (pHVol* CONST_PH_MUL) + CONST_PH_ADD ;
   
-  digitalWrite(pHEnablePin, HIGH);
+  digitalWrite(pHEnablePin, HIGH); // disable PH module power
   return(phValue); 
 }
